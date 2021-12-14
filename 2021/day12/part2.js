ece50a9f36d8eecacab9586dcb1664fedebceb7d
James Hoffmann <jamesolinhoffmann@gmail.com>
2021-12-14 00:00:38 -0600

    reformatted parser and forgot to add to part 2

--- 2021/day12/part2.js
+++ 2021/day12/part2.js
@@ -2,7 +2,16 @@ import {readFile} from '../../utilities/readFile.js'
 
 let graph = {}
 readFile()
-    
+    .split('\r\n')
+    .map(el => {
+        let tup = el.split('-')
+        return {p1: tup[0], p2: tup[1]}
+    }).forEach(el => {
+        if (!(el.p1 in graph)) graph[el.p1] = new Set()
+        if (!(el.p2 in graph)) graph[el.p2] = new Set()
+        graph[el.p1].add(el.p2)
+        if (el.p2 !== 'end' && el.p1 !== 'start') graph[el.p2].add(el.p1)
+    })
 
 const main = () => {
     let paths = []
