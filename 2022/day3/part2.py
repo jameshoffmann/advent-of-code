589513448d78f2c368c2086151f701637dd78a1e
James Hoffmann <bg@Jamess-MacBook-Pro-2.local>
2022-12-03 03:03:30 -0600

    hmmm

--- 2022/day3/part2.py
+++ 2022/day3/part2.py
@@ -1,12 +1,11 @@
 def parse(input):
-    lst = open(input).read().split("\n")
-    return list(zip(*[iter(lst)]*3))
-
+    return list(zip(*[iter([set(x) for x in open(input).read().split("\n")])]*3))
+    
 def main():
     groups = parse("input.txt")
     sum = 0
     for group in groups:
-        badge = ''.join(set(group[0]).intersection(set(group[1]).intersection(set(group[2]))))
+        badge = ''.join(group[0].intersection(group[1].intersection(group[2])))
         sum += ord(badge) - 96 if badge.islower() else ord(badge) - 38
     print(sum)
                 
